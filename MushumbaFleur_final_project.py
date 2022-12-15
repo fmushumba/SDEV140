759af8d663255daa08be8d4c2eda12dbfeca2a53
fmushumba <99782563+fmushumba@users.noreply.github.com>
2022-12-14 23:43:37 -0500

    Add files via upload

--- MushumbaFleur_final_project.py
+++ MushumbaFleur_final_project.py
@@ -1,16 +1,28 @@
 from tkinter import *
 from PIL import ImageTk, Image
 
-
+#window 1
 root = Tk()
 
 #set app title and background
 root.title("Audit+")
 root.geometry("600x400")
 root.state('zoomed')
-root.configure(bg="#90ee90"),
+root.configure(bg="#90ee90")
 
-#retrieving image
+
+#Introduction label
+label1= Label(root,
+    text=" Welcome to the auditing app that makes your life easy",
+    padx=460,
+    pady=5,
+    font=("algerian, 12")
+    
+)
+label1.grid(row=1 , column = 0, columnspan=5)
+
+
+#background image
 image1= Image.open('C:/Users/mushu/OneDrive/Documents/Python Scripts/Learning/SDEV140/tkinterapp/app_icon.jpg')
 test= ImageTk.PhotoImage(image1)
 image_label=Label(root, image= test)
@@ -18,9 +30,47 @@ image_label.image=test
 image_label.place(x=400,y=100)
 
 
-#user entry
 
-def add_user_comments():
+
+#Window2
+root2=Toplevel()
+
+#Comparison image 
+
+image2=ImageTk.PhotoImage(Image.open('C:/Users/mushu/OneDrive/Documents/Python Scripts/Learning/SDEV140/tkinterapp/dirty_factory2.png')) #first image 
+image2_label=Label(root2,image=image2,)
+image2_label.grid(row=3, column=0)
+
+image3=ImageTk.PhotoImage(Image.open('C:/Users/mushu/OneDrive/Documents/Python Scripts/Learning/SDEV140/tkinterapp/dirty factory.png')) #second image 
+image3_label=Label(root2,image=image2,)
+image3_label.grid(row=3, column=3, columnspan=3)
+
+# This function creates new window
+def imageWindow():
+    root2.title("past Images")
+    root2.geometry("200x200")
+    root2.state('zoomed')
+    root2.configure(bg="black")
+    new_window_items()  # function calls the label and quit functions
+
+
+# Function adds items to new window
+def new_window_items():    
+    label2= Label(root2,  text = 'This window shows Past Pictures', font =('calibri', 15))
+    label2.grid(row =1, column=2, columnspan=3)
+    
+     #Button to close window
+    button5=Button(root2, padx=5, pady = 30, text= 'click here to close window', command=root2.destroy)
+    button5.grid(row=5 , column = 2, columnspan=3)
+
+
+
+
+
+#Call back functions 
+    
+#user entry
+def add_user_findings():
     user_comments= Text(root,
         width=30,
         height=20,
@@ -29,56 +79,44 @@ def add_user_comments():
         fg='green')
     user_comments.grid(row=4,column=0)
 
-#labels
-label1= Label(root,
-    text=" Welcome to the auditing app that makes you life easy",
-    padx=500,
-    pady=5,
+#user recommendation entry 
+def add_user_recommendation():
+    user_comments= Text(root,
+        width=30,
+        height=20,
+        borderwidth=5,
+        bg="white",
+        fg='green')
+    user_comments.grid(row=4,column=3,columnspan=4)
     
-)
-label1.grid(row=1 , column = 0, columnspan=5)
 
-def area_click():
-    area_label= Label(root,
-        text='line 1',
-        padx=30,
-        pady=30
-    )
-    area_label.grid(row=4 , column=0) 
-    
+
+
+# Menu option to open regulation file  
 def add_GMP_Regulations():
     with open('C:/Users/mushu/Downloads/21 CFR Part 117 (up to date as of 11-30-2022).pdf','r') as file:
         file.read()
+
+#Menu option to set dark mode 
 def darkMode():
     if darkmode.get() == 1:
         root.config(background='#90ee90')
     elif darkmode.get() == 0:
         root.config(background='black')
 
-
-minimap = BooleanVar()
-minimap.set(True)
 darkmode = BooleanVar()
 darkmode.set(False)
 
-button1= Button(root,
-    padx=60,
-    pady=10,
-    text='Click here to enter findings',
-    fg="black",
-    bg="green",
-    command=add_user_comments
-)
 
-button1.grid(row=2, column= 0,)
+
 
 # Adding Menu options (areas, 
 menubar=Menu(root)
-menubar.add_command(label=" Area scores", command=lambda: print("Hello"))
-menubar.add_command(label="Images", command=lambda: print("Goodbye"))
+menubar.add_command(label=" Area scores", command=lambda: print("Model being built , SORRY!"))
+menubar.add_command(label="Images", command=imageWindow)
 
 
-
+# Adding regulations otion 
 Regulations=Menu(menubar,tearoff=False, background='#ff8000')
 Regulations.add_command(label="GMP Regulation")
 Regulations.add_command(label="Cleaning Regulation")
@@ -86,16 +124,37 @@ menubar.add_cascade(label='Regulations',underline=1, menu=Regulations)
 
 #adding dark mode view option 
 view = Menu(menubar, tearoff=0)
-view.add_checkbutton(label="show minimap", onvalue=1, offvalue=0, variable=minimap)
+
 view.add_checkbutton(label='Darkmode', onvalue=1, offvalue=0, variable=darkmode, command=darkMode)
 menubar.add_cascade(label='View', menu=view)
 
 
 root.config(menu=menubar)
 
+#buttons
 
+#Findings button
+button1= Button(root,
+    padx=60,
+    pady=10,
+    text='Click here to enter findings',
+    fg="black",
+    bg="green",
+    command=add_user_findings
+)
 
+button1.grid(row=2, column= 0,)
 
+#recommendation button
+button2= Button(root,
+    padx=60,
+    pady=10,
+    text='Click here to Enter recommendation',
+    fg="black",
+    bg="green",
+    command=add_user_recommendation
+)
+button2.grid(row=2, column= 3,columnspan=4)
 
 
 
